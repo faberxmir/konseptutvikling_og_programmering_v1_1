43db6c56aae7b9d829012475ff09c7adc02838ec
noob <geir.hilmersen@protonmail.com>
2021-03-09 12:42:53 +0100

    Added highscorelist, but needs polish

--- leksjon_DOM/eksempel/livekode/app2.js
+++ leksjon_DOM/eksempel/livekode/app2.js
@@ -1,28 +1,22 @@
 const HIGHSCOREKEY = "highscore";
+const HIGHSCORELISTLENGTH = 3;
 
-const correctAnswer = Math.round(Math.random()*100);
 const inputField = document.querySelector("input");
 const outputField = document.querySelector("#feedback");
 const timerField = document.querySelector("#timer");
+
+let correctAnswer; 
 
 let guesscounter = 0;
 let runGame = true;
 let currentTimeSecs = 0;
 let timer = timerController();
 
-let highscorelist = localStorage.getItem(HIGHSCOREKEY);
+let highscorelist;
+highscorelist = getHighScoreList();
 
-if(highscorelist === null){
-    highscorelist = [
-        {name: "AAA", time: 60, guesses: 15},
-        {name: "BBB", time: 120, guesses: 15},
-        {name: "CCC", time: 180, guesses: 15}]
-    let jsonString = JSON.stringify(highscorelist);
-    localStorage.setItem(HIGHSCOREKEY, jsonString);
-}
-
-addHighScorelist();
-restart();
+renderHighScorelist();
+initNewGame();
 
 document.addEventListener('keydown', (event) => {
     switch(event.key){
@@ -30,7 +24,7 @@ document.addEventListener('keydown', (event) => {
             makeaGuess();
             break;
         case 'Escape':
-            restart();
+            initNewGame();
             break;
         default:
             if(isNaN(parseInt(event.key))){
@@ -57,13 +51,19 @@ inputField.addEventListener('input', event => {
     }
 });
 
+function generateAnswer(){
+    const answer = Math.round(Math.random()*100);
+    console.log(answer);
+    return answer;
+}
+
 function makeaGuess(){
     ++guesscounter;
     guessedNumber = inputField.value;
     if(guessedNumber > correctAnswer){
-        feedback("your guess was to high!")
+        feedback("your guess was to high!");
     } else if(guessedNumber < correctAnswer){
-        feedback("your guess was to low!")
+        feedback("your guess was to low!");
     } else {
         win();
     }
@@ -81,24 +81,36 @@ function clearInputField(){
 function win(){
     feedback(`CORRECT! Time spent ${currentTimeSecs} seconds. Number of guesses ${guesscounter}`);
     clearInterval(timer);
-    console.log("you won!");
-    checkForHighScore("");
+    checkForHighScore();
 }
 
+//Check if the user has a highscore and put it in where it belongs
 function checkForHighScore(user){
+    console.log("checkforhighscorecalled");
+    let tempPosition;
     for(let i = 0; i < highscorelist.length; ++i){
-        if(user.guesses < highscorelist[i].guesses){
-            
-            for(let y = highscorelist.length-1; y < i; --y){
-
-            }
+        if(guesscounter < highscorelist[i].guesses){
+            tempPosition = i;
+            console.log(`user has a highscore! highscore to beat ${highscorelist[i].guesses}`);
         }
     }
+    if(typeof tempPosition !== 'undefined'){
+        let tempuser;
+        for(let i = tempPosition; i >0; --i){
+            tempuser= highscorelist[i-1]
+            highscorelist[i-1] = highscorelist[i];
+        }
+        highscorelist[tempPosition].name = "GGG";
+        highscorelist[tempPosition].guesses = 10;
+        highscorelist[tempPosition].time = 10;
+    }
+    console.log(highscorelist);
+    setHighScoreList();
+    renderHighScorelist();
 }
 
-
-
-function restart(){
+function initNewGame(){
+    generateAnswer();
     resetOutputField();
     clearInputField();
     timerField.innerText = 0;
@@ -109,14 +121,42 @@ function restart(){
 function resetOutputField(){
     outputField.innerText = "Gjett et tall mellom 1 og 100";
 }
-function addHighScorelist(){
-    const highscoreHTML = document.querySelector("#highscoreHTML").getElementsByTagName("li");
-    highscorelist = JSON.parse(highscorelist);
-    let node = highscoreHTML.firstChild;
 
-    for(let i = 0; i < highscoreHTML.length; ++i){
-        console.log(highscoreHTML[i]);
-        highscoreHTML[i].innerHTML = `${highscorelist[i].name} - guesses: ${highscorelist[i].guesses} - time: ${highscorelist[i].time}`;
+
+function setHighScoreList(){
+    localStorage.setItem(HIGHSCOREKEY, JSON.stringify(highscorelist));
+}
+
+function getHighScoreList(){
+    highScoreList = JSON.parse(localStorage.getItem(HIGHSCOREKEY));
+
+    if(highscorelist === null){
+        highscorelist = [
+            {name: "AAA", time: 60, guesses: 15},
+            {name: "BBB", time: 120, guesses: 15},
+            {name: "CCC", time: 180, guesses: 15}]
+        let jsonString = JSON.stringify(highscorelist);
+        localStorage.setItem(HIGHSCOREKEY, jsonString);
+    }
+
+    return highScoreList;
+}
+function renderHighScorelist(){
+    let highscoreHTML = document.querySelector("#highscoreHTML"); 
+    highscorelist = getHighScoreList();;
+    let element;
+    console.log(`highscorelist length: ${HIGHSCORELISTLENGTH}`);
+    for(let i = 0; i < HIGHSCORELISTLENGTH; ++i){
+        element = document.createElement("li");
+        element.innerHTML = 
+            `
+                <span class="name">${highscorelist[i].name}</span> 
+                <span class="guesses"> guesses: ${highscorelist[i].guesses}</span> 
+                <span class="time"> time: ${highscorelist[i].time} sec</span>
+            `
+            ;
+        
+        highscoreHTML.appendChild( element );
     }
 }
 
